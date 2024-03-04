d6fae3b6c45ec6f34c0c74cf011b03482f2fb513
JuHyun <68935085+beorrol@users.noreply.github.com>
2024-03-04 20:31:35 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int arr[7] = { 1, 1, 2, 2, 2, 8 };
+    int a;
+
+    for (int i = 0; i < 6; i++) {
+        scanf("%d", &a);
+        printf("%d ", arr[i] - a);
+    }
+
+    return 0;
+}
